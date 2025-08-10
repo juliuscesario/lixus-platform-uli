a8948244b691ee205fb73e3638701a9d40678ea4
panjul <julius@lixus.id>
2025-08-10 16:21:28 +0700

    fixing footer copy

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
                 <div className="text-center">
                     <p className="text-lg font-bold"><a href="https://lixus.id" target='_blank'>Lixus.id</a></p>
-                    <p className="mt-2 text-sm text-gray-400">Platform Komunitas Influencer Terdepan.</p>
+                    <p className="mt-2 text-sm text-gray-400">Platform Komunitas Influencer .</p>
                     <p className="mt-2 text-sm text-gray-400">&copy; {new Date().getFullYear()} PT Lintas Ekspansi Usaha. All rights reserved.</p>
                 </div>
             </div>
