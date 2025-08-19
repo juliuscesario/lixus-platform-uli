370747e977bdaac94f59f05f1bdcf5c266e248ec
Panjul <julius.cesario17@hotmail.com>
2025-08-19 16:19:37 +0700

    remove console.log

--- resources/js/pages/admin/CampaignPostsPage.jsx
+++ resources/js/pages/admin/CampaignPostsPage.jsx
@@ -93,9 +93,6 @@ export default function CampaignPostsPage() {
             }
         }
         
-        console.log('Fetching posts for campaignId:', campaignId);
-        console.log('With queryParams:', queryParams.toString());
-        
         try {
             // Always use getAdminCampaignPosts with constructed query parameters
             const response = await apiService(auth).getAdminCampaignPosts(campaignId, queryParams.toString());
